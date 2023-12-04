ca2f9b95d377a58b9e862d3d71892fd2ee4f7cf5
MariaAbh <maria.abouhaidar1@gmail.com>
2023-12-04 19:01:08 +0100

    Add tri_insert function

--- insertion_sort.py
+++ insertion_sort.py
@@ -13,8 +13,31 @@ def inserer(l,start,end):
     else:
         return inserer(l,start+1,end)
     
+def tri_insertion(l,start,end):
+    if start == end+1:
+        return 
+    if start == 0:
+        return inserer(l,start+1,end)
+    current = l[start]
+    prev = l[start-1]
+    if current < prev:
+        temp = prev
+        l[start-1] = current
+        l[start] = temp
+        return inserer(l,start-1,end)
+    else:
+        return inserer(l,start+1,end)
 
-l = [6,4,1,3,5,2]
 start = 0
 end = 5
-print(inserer(l,start,end))
+
+l = [6,4,1,3,5,2]
+print('inserer:',inserer(l,start,end))
+
+l1 = [6,4,1,3,5,2]
+l1.sort()
+print('l.sort():',l1)
+
+l2 = [6,4,1,3,5,2]
+tri_insertion(l2,start,end)
+print('tri_insertion:',l2)
